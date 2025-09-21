15a45dd6997030963f3c0928c21a95c71348a7ef
Stanley <owarieta24@outlook.com>
2025-09-21 15:07:16 +0100

    Fix: status code to 200 for login endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -89,7 +89,7 @@ export const login = async (req, res) => {
 
     generateToken(user._id, res);
 
-    res.status(201).json({
+    res.status(200).json({
         _id: user._id,
         fullName: user.fullname,
         email: user.email,
